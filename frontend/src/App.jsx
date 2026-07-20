cea2f25f2c5468cab71cce5eb852177f1486d4ad
songyc04 <songyc0324@gmail.com>
2026-07-20 17:08:10 +0900

    mid upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react"
+
 import LeafletTest from "./components/LeafletTest"
+import StadiumSelector from "./components/StadiumSelector"
 
 
-function App() {
+import europeStadiums from "@data/europe.json"
+import asiaStadiums from "@data/asia.json"
+import africaStadiums from "@data/africa.json"
+import northcentralamericaStadiums from "@data/northcentralamerica.json"
+import oceaniaStadiums from "@data/oceania.json"
+import southamericaStadiums from "@data/southamerica.json"
+
+
+const App = () => {
+  const [stadium, setStadium] = useState("");
+  const [latitude, setLatitude] = useState(0.0);
+  const [longitude, setLongitude] = useState(0.0);
+
+  useEffect(() => {
+    setStadium("Wembley Stadium");
+    
+  }, []);
+
 
   return (
     <>
-      <LeafletTest />
+      {/*{euStadiums.map((stadium) => {
+        return (<h1 key={stadium.id}>{stadium.stadium}</h1>)
+      })}*/}
+      <StadiumSelector onChange={setStadium} />
+      <h1>{stadium}</h1>
     </>
   )
 }
--- frontend/src/components/LeafletTest.jsx
+++ frontend/src/components/LeafletTest.jsx
@@ -1,16 +1,23 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet"
+import L from 'leaflet'
 
-const LeafletTest = () => {
+
+const stadiumIcon = L.divIcon({
+  html: `<div style="font-size: 28px;">⚽</div>`,
+  className: '', // 기본 Leaflet 클래스(회색 배경 박스) 제거용, 비워두는 게 핵심
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+})
+
+const LeafletTest = ( { latitude, longitude, stadiumName } ) => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
+    <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false} style={{ height: '500px', width: '100%' }}>
+      <TileLayer 
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={[latitude, longitude]} icon={stadiumIcon}>
+        <Popup>{stadiumName}</Popup>
       </Marker>
     </MapContainer>
   );
