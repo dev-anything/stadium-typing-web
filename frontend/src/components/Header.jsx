e4817d40e6da23c0e8dbb26334e0306817c4587f
songyc04 <songyc0324@gmail.com>
2026-07-23 10:46:09 +0900

    Instagram link added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import icon_instagram from "@assets/icon_instagram.png";
+
 const Header = () => {
   return (
     <header className="w-2/3 mx-auto flex items-center justify-between px-6 py-6">
@@ -9,6 +11,13 @@ const Header = () => {
       >
         STADIO TYPE
       </Link>
+      <a 
+        href="https://www.instagram.com/dev_anything_?igsh=dmg2dWhrZXNocGt1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="w-8" src={icon_instagram} />
+      </a>
       <span className="font-ibm-plex-mono text-[10px] tracking-widest text-[#5C6F65]">
         WORLD TOUR
       </span>
