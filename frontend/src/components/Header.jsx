141b8745c4ef9590cf310b0d7e21633846c64d51
songyc04 <songyc0324@gmail.com>
2026-07-24 23:25:56 +0900

    Header modify

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import icon_instagram from "@assets/icon_instagram.png";
 
 const Header = () => {
   return (
-    <header className="w-2/3 mx-auto flex items-center justify-between px-6 py-6">
+    <header className="w-6/10 mx-auto flex items-center justify-between px-6 py-6">
       <Link
         to="/"
         className="font-display text-[25px] tracking-wide"
@@ -18,9 +18,9 @@ const Header = () => {
       >
         <img className="w-8" src={icon_instagram} />
       </a>
-      <span className="font-ibm-plex-mono text-[10px] tracking-widest text-[#5C6F65]">
+      {/*<span className="font-ibm-plex-mono text-[10px] tracking-widest text-[#5C6F65]">
         WORLD TOUR
-      </span>
+      </span>*/}
     </header>
   );
 }
