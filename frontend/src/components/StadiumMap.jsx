df3f9e0e0be9d6023d8930cc599e990b8415c097
songyc04 <songyc0324@gmail.com>
2026-07-28 17:24:43 +0900

    Map

--- frontend/src/components/StadiumMap.jsx
+++ frontend/src/components/StadiumMap.jsx
@@ -111,9 +111,8 @@ const StadiumMap = ( { latitude, longitude, stadiumName } ) => {
         className="w-full"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-
-          url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {/*<ZoomControl position="bottomright" />*/}
         <MapUpdater latitude={latitude} longitude={longitude} />
